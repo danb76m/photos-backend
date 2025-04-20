d462db12c781453d8b7ae52c359fe2fcf19d5358
Danielle Baumann <daniel_baumann@hotmail.co.uk>
2025-04-21 00:28:39 +0100

    removed main cos im dumb updated tests

--- src/test/java/me/danb76/photos/PhotosApplicationTests.java
+++ src/test/java/me/danb76/photos/PhotosApplicationTests.java
@@ -2,12 +2,41 @@ package me.danb76.photos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.mockito.Mockito;
+import javax.sql.DataSource;
+import jakarta.persistence.EntityManagerFactory;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration.class
+})
 class PhotosApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Configuration
+	static class TestConfig {
+		@Bean
+		DataSource dataSource() {
+			return Mockito.mock(DataSource.class);
+		}
+
+		@Bean
+		EntityManagerFactory entityManagerFactory() {
+			return Mockito.mock(EntityManagerFactory.class);
+		}
 	}
 
+	@Test
+	void contextLoads(ApplicationContext context) {
+
+	}
 }
